5514d976b740c7ac40cef98acee8cfcdc54bafe3
vignesh balusu <vigneshbalusuu@gmail.com>
2025-06-29 13:23:11 +0530

    final commit try2

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ def predict():
         return f"❌ Internal error during prediction: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
